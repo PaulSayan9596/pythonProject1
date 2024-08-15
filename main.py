7fff172539245714ee981ed6e4b0981160e50d60
Sayan Paul <sayanpaul555yes@gmail.com>
2024-08-15 21:21:31 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+a=int(input("Enter the row number: "))
+k=0
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        k=k+1
+        print(k,end=" ")
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
